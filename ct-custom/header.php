665e9eb5c4f4096a85d4165b0249e2ac9a82df7b
Hyppolite <hyppolitak@gmail.com>
2024-03-27 10:22:53 +0100

    Customize the header template

--- ct-custom/header.php
+++ ct-custom/header.php
@@ -25,34 +25,58 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'ct-custom' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$ct_custom_description = get_bloginfo( 'description', 'display' );
-			if ( $ct_custom_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $ct_custom_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+		<div class="ct-top">
+			<div class="ct-center">
+				<div class="ct-callus">
+					<span class="ct-color-darkorange">CALL US NOW!</span>&ensp;
+					<span class="ct-color-white"><?php echo get_option('phone'); ?></span>
+				</div>
+				<div class="ct-auth">
+					<span class="ct-color-darkorange"><a href="#">LOGIN</a></span>&ensp;
+					<span class="ct-color-white"><a href="#">SIGNUP</a></span>
+				</div>
+			</div>
+		</div>
+		<div class="ct-topnav">
+			<div class="ct-center">
+				<div class="site-branding">
+					<?php
+					if (get_option('logo')) :
+						?>
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="custom-logo-link" rel="home">
+							<img src="<?php echo get_option('logo'); ?>" class="custom-logo" alt="<?php echo get_bloginfo( 'name' ); ?>">
+						</a>
+						<?php
+					else :
+						the_custom_logo();
+					endif;
+					if ( is_front_page() && is_home() ) :
+						?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<?php
+					else :
+						?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+						<?php
+					endif;
+					$ct_custom_description = get_bloginfo( 'description', 'display' );
+					if ( $ct_custom_description || is_customize_preview() ) :
+						?>
+						<p class="site-description"><?php echo $ct_custom_description; /* WPCS: xss ok. */ ?></p>
+					<?php endif; ?>
+				</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'ct-custom' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
-		</nav><!-- #site-navigation -->
+				<nav id="site-navigation" class="main-navigation">
+					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'MENU', 'ct-custom' ); ?></button>
+					<?php
+					wp_nav_menu( array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					) );
+					?>
+				</nav><!-- #site-navigation -->
+			</div>
+		</div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
