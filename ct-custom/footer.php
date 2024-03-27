09bc44218b41014f7cbdf8b21eace6bfe8e3525d
Hyppolite <hyppolitak@gmail.com>
2024-03-27 10:57:29 +0100

    Customize the footer template

--- ct-custom/footer.php
+++ ct-custom/footer.php
@@ -14,19 +14,35 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'ct-custom' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'ct-custom' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'ct-custom' ), 'ct-custom', '<a href="https://coalitiontechnologies.com/">Coalition Technologies</a>' );
-				?>
-		</div><!-- .site-info -->
+		<div class="ct-footer">
+			<div class="ct-contact">
+				<span class="ct-title">CONTACT US</span>
+				<div class="ct-line"></div>
+			</div>
+			<div class="ct-reachus">
+				<span class="ct-title">REACH US</span>
+				<div class="ct-line"></div>
+				<div class="ct-break ct-info">
+					<?php echo get_bloginfo( 'name' ); ?>
+					<br><br>
+					<?php echo get_option('address'); ?>
+					<br><br>
+					Phone: <?php echo get_option('phone'); ?>
+					<br>
+					Fax: <?php echo get_option('fax'); ?>
+				</div>
+				<div class="ct-break">
+					<span class="ct-social"><a href="<?php echo get_option('facebook_url'); ?>" rel="noopener noreferrer" target=_blank>
+						<i class="fa-brands fa-square-facebook fa-2xl"></i></a></span>
+					<span class="ct-social"><a href="<?php echo get_option('twitter_url'); ?>" rel="noopener noreferrer" target=_blank>
+						<i class="fa-brands fa-square-twitter fa-2xl"></i></a></span>
+					<span class="ct-social"><a href="<?php echo get_option('linkedin_url'); ?>" rel="noopener noreferrer" target=_blank>
+						<i class="fa-brands fa-linkedin fa-2xl"></i></a></span>
+					<span class="ct-social"><a href="<?php echo get_option('pinterest_url'); ?>" rel="noopener noreferrer" target=_blank>
+						<i class="fa-brands fa-pinterest fa-2xl"></i></a></span>
+				</div>
+			</div>
+		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
